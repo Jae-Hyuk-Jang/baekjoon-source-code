a69dfec78e74ba06272bf476008e4989d74d1f47
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-09-24 08:28:41 +0900

    commit log

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -5,26 +5,10 @@ int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	long long int a, b; cin >> a >> b;
-	if (a > b) {
-		long long int tmp = b;
-		b = a;
-		a = tmp;
-
-		cout << b - a - 1 << '\n';
-		while (a < b - 1) {
-			cout << ++a << ' ';
-		}
+	int t; cin >> t;
+	while (t--) {
+		long long int a, b; cin >> a >> b;
+		cout << a + b << '\n';
 	}
-	else if (a == b) {
-		cout << 0;
-	}
-	else {
-		cout << b - a - 1 << '\n';
-		while (a < b - 1) {
-			cout << ++a << ' ';
-		}
-	}
-
 	return 0;
 }
