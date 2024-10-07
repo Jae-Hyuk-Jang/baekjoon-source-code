823fc511a9514a0be8006936f55019f81c75c7a1
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-10-08 07:47:52 +0900

    basic commit

--- basic/main.cpp
+++ basic/main.cpp
@@ -1,67 +1,28 @@
 #include<bits/stdc++.h>
-#include<unordered_map>
-#include<unordered_set>
-#define MX 500001
-#define MOD 1000000000
 using namespace std;
-
-
-void parse(string& tmp, deque<int>& d) {
-	int cur = 0;
-	for (int i = 1; i + 1 < tmp.size(); i++) {
-		if (tmp[i] == ',') {
-			d.push_back(cur);
-			cur = 0;
-		}
-		else {
-			cur = 10 * cur + (tmp[i] - '0');
-		}
-	}
-	if (cur != 0) d.push_back(cur);
-}
-
-void print(deque<int>& d) {
-	cout << '[';
-	for (int i = 0; i < d.size(); i++) {
-		cout << d[i];
-		if (i + 1 != d.size()) cout << ',';
-	}
-	cout << "]\n";
-}
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int t; cin >> t;
-	while (t--) {
-		deque<int> dq;
-		int rev = 0;
-		int n;
-		bool isWrong = false;
-		string query, tmp;
-		cin >> query >> n >> tmp;
-
-		parse(tmp, dq);
-		for (auto c : query) {
-			if (c == 'R') rev = 1 - rev;
-			else {
-				if (dq.empty()) {
-					isWrong = true;
-					break;
+	int T; cin >> T;
+	cin.ignore();
+	while (T--) {
+		string s = "";
+		getline(cin, s);
+		s+= ' ';
+		stack<char> st;
+		for (const auto& i : s) {
+			if (i == ' ') {
+				while (!st.empty()) {
+					cout << st.top();
+					st.pop();
 				}
-				if (!rev) dq.pop_front();
-				else dq.pop_back();
+				cout << i;
 			}
-		}
-
-		if (isWrong) cout << "error\n";
-		else {
-			if (rev)  reverse(dq.begin(), dq.end());
-			print(dq);
+			else st.push(i);
 		}
 	}
-	
 
 	return 0;
 }
