850a19994a2779204f708b259067471a311a0106
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-10-24 17:20:29 +0900

    platinum commit

---
+++ 3015/main.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ull;
+
+ull num;
+stack<pair<ull, int>> st;
+ull ans = 0;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N; cin >> N;
+	while (N--) {
+		cin >> num;
+		int cnt = 1;
+		while (!st.empty() && st.top().first <= num) {
+			ans += st.top().second;
+			if (st.top().first == num) cnt += st.top().second;
+			st.pop();
+		}
+		if (!st.empty()) ans++;
+		st.push({ num, cnt });
+	}
+	
+	cout << ans;
+
+	return 0;
+}
