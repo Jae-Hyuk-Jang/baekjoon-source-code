f8c7244ebb5a0ba7dc0e746236f9ba911892819b
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-10-31 12:25:48 +0900

    bronze commit

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -1,31 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define MX 1001
-double arr[MX];
-
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n; 
-	cin >> n;
-	for (int i = 0; i < n; i++) cin >> arr[i];
-	sort(arr, arr + n);
-	double m = arr[n - 1];
+	int t; cin >> t;
+	int h, w, n, fl, no;
+	string ans, nostr;
+	
+	while (t--) {
+		cin >> h >> w >> n;
+		no = (n - 1) / h + 1;
+		fl = (n - 1) % h + 1;
 
-	for (int i = 0; i < n; i++) {
-		arr[i] = (arr[i] / m) * 100;
+		nostr = to_string(no);
+		if (nostr.size() == 1) nostr = '0' + nostr;
+		ans = to_string(fl) + nostr;
+		cout << ans << '\n';
 	}
 
-	double avg = 0;
-	for (int i = 0; i < n; i++) {
-		avg += arr[i];
-	}
-	avg /= n;
-
-	cout.precision(10);
-	cout << avg;
-    
 	return 0;
 }
