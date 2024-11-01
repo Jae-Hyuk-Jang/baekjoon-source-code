6719f69467dca542d399b02f57fd99926546bd5a
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-11-01 13:44:21 +0900

    bronze commit

--- 13144/main.cpp
+++ 13144/main.cpp
@@ -1,11 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int MX = 100002;
+int arr[MX];
+bool processed[MX];
+long long ans = 0;
+
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) cin >> arr[i];
 
+	processed[arr[0]] = 1;
+	int en = 0;
+	for (int st = 0; st < n; st++) {
+		while (en < n - 1 && !processed[arr[en + 1]]) {
+			en++;
+			processed[arr[en]] = 1;
+		}
+		ans += (en - st + 1);
+		processed[arr[st]] = 0;
+	}
+	cout << ans;
 
 	return 0; 
 }
--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -1,24 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define MX 1000000
+bool nums[MX+1];
+
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int t; cin >> t;
-	int h, w, n, fl, no;
-	string ans, nostr;
-	
-	while (t--) {
-		cin >> h >> w >> n;
-		no = (n - 1) / h + 1;
-		fl = (n - 1) % h + 1;
-
-		nostr = to_string(no);
-		if (nostr.size() == 1) nostr = '0' + nostr;
-		ans = to_string(fl) + nostr;
-		cout << ans << '\n';
+	fill(nums + 1, nums + MX + 1, true);
+	nums[0] = nums[1] = 0;
+	for (int i = 2; i*i <= MX; i++) {
+		if (nums[i]) {
+			for (int k = i * i; k <= MX; k += i) {
+				nums[k] = false;
+			}
+		}
 	}
+
+	int m, n; cin >> m >> n;
+	for (int i = m; i <= n; i++) {
+		if (nums[i]) cout << i << '\n';
+	}
+
 
 	return 0;
 }
