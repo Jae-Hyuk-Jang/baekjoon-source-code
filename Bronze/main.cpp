f597d0c5e39849b841aa55e5211131c338647016
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-11-08 13:01:06 +0900

    bronze commit

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -1,15 +1,11 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-
-	int t = 1;
-	while (1) {
-		int L, P, V; cin >> L >> P >> V;
-		if (V == 0)break;
-		cout << "Case " << t++ << ": " << V / P * L + min(V % P, L) << '\n';
-	}
-	return 0;
+    while (1) {
+        int a, b; cin >> a >> b;
+        if (a == 0 && b == 0) break;
+        cout << a + b << '\n';
+    }
+    return 0;
 }
