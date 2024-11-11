1d03db7ce690041a0507049d7632abce80890562
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-11-12 08:01:02 +0900

    bronze commit

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -5,12 +5,14 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, cnt = 0;
-    cin >> n;
-    n = (~n + 1) ^ n;
-    for (int i = 0; i < 32; i++)
-        if ((n >> i) & 1) cnt++;
-    cout << cnt;
-
-    return 0;
+    int t; cin >> t;
+    int mini = 1e9;
+    int maxi = -1e9;
+    while (t--) {
+        int n; cin >> n;
+        mini = min(mini, n);
+        maxi = max(maxi, n);
+    }
+    cout << mini << ' ' << maxi;
+    return 0; 
 }
