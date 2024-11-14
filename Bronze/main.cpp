22eb84d72ad2f791ad2bced2bff5470754cb65a1
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-11-14 14:38:04 +0900

    bronze commit

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -5,10 +5,15 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-	int t; string s;
-	cin >> t >> s;
-	int sum = 0;
-	for (int i = 0; i < t; i++) sum += s[i] - '0';
-	cout << sum;
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        string s; cin >> s;
+        int len = s.size();
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < n; j++) cout << s[i];
+        }
+        cout << '\n';
+    }
     return 0; 
 }
