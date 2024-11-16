8acf8d1cd23343d454aab018d1402b23c32f8d1b
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-11-16 11:00:14 +0900

    base commit

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -5,9 +5,12 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n; cin >> n;
-    for (int i = 1; i <= 9; i++) {
-        cout << n << " * " << i << " = " << n * i << '\n';
+    int t; cin >> t;
+    while (t--) {
+        int a, b, result = 1;
+        cin >> a >> b;
+        while (b--) result = (result * a) % 10;
+        cout << ((!result) ? 10 : result) << '\n';
     }
-    return 0; 
+    return 0;
 }
