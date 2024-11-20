153e7db0c21d89aee1dc06c2ce8ab0a055d1939d
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-11-20 15:07:55 +0900

    silver commit

--- Silver/main.cpp
+++ Silver/main.cpp
@@ -1,17 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n; cin >> n;
-	int ans = 0;
-	while (n) {
-		ans += (n / 5);
-		n /= 5;
-	}
-	cout << ans;
+    std::function<ll(ll, ll, ll)> POW = [&](ll a, ll b, ll m) -> ll {
+        if (b == 1) return a % m;
+
+        ll val = POW(a, b / 2, m);
+
+        val = val * val % m;  
+
+        if (b % 2 == 0) return val;  
+        return val * a % m;  
+    };
+    ll a, b, c; cin >> a >> b >> c;
+    cout << POW(a, b, c);
 
 	return 0;
 }
