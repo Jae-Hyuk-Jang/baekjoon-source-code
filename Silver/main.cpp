0d4d61c8f854b77f553c47d78bd5b517153651bb
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-11-21 14:06:30 +0900

    silver commit

--- Silver/main.cpp
+++ Silver/main.cpp
@@ -1,24 +1,39 @@
 #include<bits/stdc++.h>
+#define MX 10
 using namespace std;
 
-using ll = long long;
+int N, M;
+int arr[MX];
+vector<int> res;
+bool check[MX];
+
+void dfs() {
+	if (res.size() == M) {
+		for (auto a : res) cout << a << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (check[i]) continue;
+		check[i] = true;
+		res.push_back(arr[i]);
+		dfs();
+		check[i] = false;
+		res.pop_back();
+	}
+}
+
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-    std::function<ll(ll, ll, ll)> POW = [&](ll a, ll b, ll m) -> ll {
-        if (b == 1) return a % m;
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) cin >> arr[i];
+	sort(arr + 1, arr + 1 + N);
 
-        ll val = POW(a, b / 2, m);
-
-        val = val * val % m;  
-
-        if (b % 2 == 0) return val;  
-        return val * a % m;  
-    };
-    ll a, b, c; cin >> a >> b >> c;
-    cout << POW(a, b, c);
+	dfs();
 
 	return 0;
 }
