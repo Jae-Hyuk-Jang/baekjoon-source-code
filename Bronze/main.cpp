6697102ab4584fa8e1f9891847fcaa9ce57d1e7a
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-12-03 14:01:04 +0900

    basiuc commit

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -1,29 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int arr[101];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-	long int a, b, c;
-	int arr[3];
-	while (1)
-	{
-		cin >> a; arr[0] = a;
-		cin >> b; arr[1] = b;
-		cin >> c; arr[2] = c;
+	int n = 0, m = 0, sum = 0, result = 0;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> arr[i];
 
-		sort(arr, arr + 3);
-		a = arr[0], b = arr[1], c = arr[2];
-		if (a == 0 && b == 0 && c == 0) break;
-		else if (((a * a) + (b * b) == (c * c)) && (a > 0 && b > 0 && c > 0))//1 0 1일때의 반례 방지
-			cout << "right" << endl;
-		else
-			cout << "wrong" << endl;
+	for (int i = 0; i < n - 2; i++) {
+		for (int j = i + 1; j < n - 1; j++)
+		{
+			for (int k = j + 1; k < n; k++) {
+
+				sum = arr[k] + arr[j] + arr[i];
+				if (sum <= m && sum > result) {
+					result = sum;
+				}
+			}
+		}
 
 	}
-    
+
+
+	cout << result << '\n';
+
     return 0;
 }
 
