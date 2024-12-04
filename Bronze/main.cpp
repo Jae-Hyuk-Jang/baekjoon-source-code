d335b56db80e0e10a5cfd64c85a0f48553145436
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-12-04 22:43:12 +0900

    basic commit

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -1,34 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int arr[101];
+int sum, temp, result, N, cnt = 0;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-	int n = 0, m = 0, sum = 0, result = 0;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) cin >> arr[i];
+    cin >> N; temp = N;
+    while (temp > 0) {
+        cnt++;
+        temp /= 10;
+    }
 
-	for (int i = 0; i < n - 2; i++) {
-		for (int j = i + 1; j < n - 1; j++)
-		{
-			for (int k = j + 1; k < n; k++) {
-
-				sum = arr[k] + arr[j] + arr[i];
-				if (sum <= m && sum > result) {
-					result = sum;
-				}
-			}
-		}
-
-	}
-
-
-	cout << result << '\n';
+    for (int i = N - 9 * cnt; i < N; i++) {
+        if (i < 0) i = 1;
+        sum = 0;
+        temp = i;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        if (i + sum == N) {
+            cout << i;
+            return 0;
+        }
+    }
+    cout << '0';
 
     return 0;
 }
-
 
