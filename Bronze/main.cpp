6b7a68a3ec77a4be4c6a37a7a94e4db60419aef1
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-12-05 23:32:35 +0900

    basic commit

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -1,33 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int sum, temp, result, N, cnt = 0;
+
+long long mod_pow(int r, const int& i, int m) {
+    long long ans = 1;
+    for (int j = 0; j < i; j++) ans = ans * r % m;
+    return ans % m;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    cin >> N; temp = N;
-    while (temp > 0) {
-        cnt++;
-        temp /= 10;
+    long long l, m = 1234567891, r = 31, h = 0;
+    char c[50];
+
+    cin >> l;
+    for (int i = 0; i < l; i++) {
+        cin >> c[i];
+        c[i] -= 96;
     }
 
-    for (int i = N - 9 * cnt; i < N; i++) {
-        if (i < 0) i = 1;
-        sum = 0;
-        temp = i;
-        while (temp > 0) {
-            sum += temp % 10;
-            temp /= 10;
-        }
-        if (i + sum == N) {
-            cout << i;
-            return 0;
-        }
-    }
-    cout << '0';
+    for (int i = 0; i < l; i++) h += c[i] * mod_pow(r, i, m) % m;
+
+    h %= m;
+    cout << h;
 
     return 0;
 }
+
+
+
 
