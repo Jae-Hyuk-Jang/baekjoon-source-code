edccfb9ce15b8bffcc45e99d8c4a3ea4cb054bfa
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-12-06 08:23:05 +0900

    basic commit

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -2,32 +2,28 @@
 using namespace std;
 
 
-long long mod_pow(int r, const int& i, int m) {
-    long long ans = 1;
-    for (int j = 0; j < i; j++) ans = ans * r % m;
-    return ans % m;
-}
-
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    long long l, m = 1234567891, r = 31, h = 0;
-    char c[50];
+	string str1, str2, str3;
+	cin >> str1 >> str2 >> str3;
 
-    cin >> l;
-    for (int i = 0; i < l; i++) {
-        cin >> c[i];
-        c[i] -= 96;
-    }
-
-    for (int i = 0; i < l; i++) h += c[i] * mod_pow(r, i, m) % m;
-
-    h %= m;
-    cout << h;
+	auto print = [](int N) {
+		if (N % 3 == 0 && N % 5 == 0) cout << "FizzBuzz" << "\n";
+		else if (N % 3 == 0) cout << "Fizz" << "\n";
+		else if (N % 5 == 0) cout << "Buzz" << "\n";
+		else cout << N << "\n";
+		};
+	if (str3 != "FizzBuzz" && str3 != "Fizz" && str3 != "Buzz") print(stoi(str3) + 1);
+	else if (str2 != "FizzBuzz" && str2 != "Fizz" && str2 != "Buzz") print(stoi(str2) + 2);
+	else print(stoi(str1) + 3);
 
     return 0;
 }
+
+
+
 
 
 
