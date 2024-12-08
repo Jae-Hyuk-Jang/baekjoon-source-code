e1bca62cb2ff85e934b8103537130d24bd482699
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-12-08 12:18:19 +0900

    baisc commit

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -1,28 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int arr[15][15];
+
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int T; cin >> T;
-    int k, n;
-
-    for (int i = 1; i < 15; i++) arr[0][i] = i;
-
-    for (int i = 1; i < 15; i++) {
-        for (int j = 1; j < 15; j++) {
-            arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
+    string str;
+    int cnt = 0;
+    int n; cin >> n;
+    for (int title = 666; ; title++) {
+        str = to_string(title);
+        if (str.find("666") != -1) {
+            cnt++;
+            if (cnt == n) {
+                cout << str;
+                return 0;
+            }
         }
     }
-
-    while (T--) {
-        cin >> k >> n;
-        cout << arr[k][n] << '\n';
-    }
-
+    
     return 0;
 }
 
