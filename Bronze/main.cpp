4bcdb9c26007d2ea95d10fb001c3aea7a6554a47
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2024-12-10 18:35:33 +0900

    basic commit

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -1,24 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+char board[50][50];
+int n, m, ans, cnt;
+
+int getCount(int startX, int startY) {
+    int turn = 0, temp = n * m;
+
+    for (int start = 1; start <= 2; start++) {
+        int count = 0;
+        for (int i = startX; i < startX + 8; i++) {
+            turn = start;
+            for (int j = startY; j < startY + 8; j++) {
+                if (turn == 1) {
+                    if (board[i][j] != 'W') count++;
+                    turn = 2;
+                }
+                else if (turn == 2) {
+                    if (board[i][j] != 'B') count++;
+                    turn = 1;
+                }
+            }
+            if (start == 1) start = 2;
+            else start = 1;
+        } 
+        temp = min(temp, count);
+    }
+    return temp;
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    string str;
-    int cnt = 0;
-    int n; cin >> n;
-    for (int title = 666; ; title++) {
-        str = to_string(title);
-        if (str.find("666") != -1) {
-            cnt++;
-            if (cnt == n) {
-                cout << str;
-                return 0;
-            }
+    cin >> n >> m;
+    ans = n * m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
         }
     }
-    
+
+    for (int i = 0; i <= n - 8; i++) {
+        for (int j = 0; j <= m - 8; j++) {
+            cnt = getCount(i, j);
+            ans = min(ans, cnt);
+        }
+    }
+    cout << ans;
+
     return 0;
 }
 
