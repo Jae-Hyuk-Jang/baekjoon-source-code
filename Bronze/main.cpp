66743790237d35464bdec81bd4def3de09ba7a79
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2025-01-09 17:58:25 +0900

    study log

--- Bronze/main.cpp
+++ Bronze/main.cpp
@@ -1,60 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define MX 16
 using namespace std;
 
-char board[50][50];
-int n, m, ans, cnt;
-
-int getCount(int startX, int startY) {
-    int turn = 0, temp = n * m;
-
-    for (int start = 1; start <= 2; start++) {
-        int count = 0;
-        for (int i = startX; i < startX + 8; i++) {
-            turn = start;
-            for (int j = startY; j < startY + 8; j++) {
-                if (turn == 1) {
-                    if (board[i][j] != 'W') count++;
-                    turn = 2;
-                }
-                else if (turn == 2) {
-                    if (board[i][j] != 'B') count++;
-                    turn = 1;
-                }
-            }
-            if (start == 1) start = 2;
-            else start = 1;
-        } 
-        temp = min(temp, count);
-    }
-    return temp;
-}
+char c[5][MX];
+int arr[5];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    cin >> n >> m;
-    ans = n * m;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> board[i][j];
+    string s;
+    for (int i = 0; i < 5; i++) {
+        cin >> s;
+        arr[i] = s.length();
+        for (int j = 0; j < arr[i]; j++) {
+            c[i][j] = s[j];
         }
     }
 
-    for (int i = 0; i <= n - 8; i++) {
-        for (int j = 0; j <= m - 8; j++) {
-            cnt = getCount(i, j);
-            ans = min(ans, cnt);
+    for (int j = 0; j < MX; j++) {
+        for (int i = 0; i < 5; i++) {
+            if (arr[i] <= j) continue;
+            cout << c[i][j];
         }
     }
-    cout << ans;
 
     return 0;
 }
-
-
-
-
-
 
 
--- Silver/main.cpp
+++ Silver/main.cpp
@@ -1,12 +1,32 @@
 #include<bits/stdc++.h>
+#define MX 101
 using namespace std;
+
+bool mat[MX][MX];
+int N;
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	long long n, ans = 0; cin >> n;
-	for (int i = 1; i <= n; i *= 10) ans += (n - i + 1);
-	cout << ans;
+	cin >> N;
+	int x = 0, y = 0;
+	for (int n = 0; n < N; n++) {
+		cin >> x >> y;
+		for (int i = x; i < x + 10; i++) {
+			for (int j = y; j < y + 10; j++) {
+				mat[i][j] = 1;
+			}
+		}
+	}
+
+	int area = 0;
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < 100; j++) {
+			if (mat[i][j]) area += 1;
+		}
+	}
+	cout << area;
+	
 	return 0;
 }
